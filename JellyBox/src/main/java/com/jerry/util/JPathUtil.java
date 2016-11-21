be276f4ffbf5480fd247a7be7f16da9e2c0083c3
jerryDev <jerryDev@218.38.137.28>
2016-11-22 00:32:50 +0900

    first commit!!

---
+++ JellyBox/src/main/java/com/jerry/util/JPathUtil.java
@@ -0,0 +1,5 @@
+package com.jerry.util;
+
+public class JPathUtil {
+
+}
---
+++ JellyBox/src/test/java/com/jerry/util/JPathUtilTest.java
@@ -0,0 +1,26 @@
+package com.jerry.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JPathUtilTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		JPathUtil util = new JPathUtil();
+		fail("Not 11 yet implemented");
+		
+	}
+
+}
