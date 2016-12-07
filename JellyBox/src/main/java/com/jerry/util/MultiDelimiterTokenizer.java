2f818710e8a463e95e6c9ecbc874cc80caa70288
jerryDev <pigeonwind@gmail.com>
2016-12-07 10:38:03 +0900

    Tokenizer 개선

--- JellyBox/src/main/java/com/jerry/util/MultiDelimiterTokenizer.java
+++ JellyBox/src/main/java/com/jerry/util/MultiDelimiterTokenizer.java
@@ -1,6 +1,8 @@
 package com.jerry.util;
 
 import java.util.StringTokenizer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 /**
  * @author jerry
@@ -9,43 +11,39 @@ import java.util.StringTokenizer;
 public class MultiDelimiterTokenizer {
 	private int tokenCount;
 	private String surplusString;
+	BiFunction <String,Character,Integer> getIndexOfFromZero;
 	
 	public MultiDelimiterTokenizer(String surplusString) {
 		this.surplusString = surplusString;
 		this.tokenCount=0;
+		getIndexOfFromZero = (String target, Character character) -> target.indexOf(character );
 	}
+
 	public String pop(String startDelimiter, String endDelimter) {
-		String targetString,result;
+		String targetString,head,result,tail;
 		targetString =  surplusString;
-		{
-			int startDelimiterOffset = targetString.indexOf(startDelimiter.charAt(0));
-			int endelimiterOffset = targetString.indexOf(endDelimter.charAt(0));
-			result = targetString.substring(startDelimiterOffset+1, endelimiterOffset);
-		}
-		targetString = targetString.replace(result, "");
-		System.out.println(targetString);
-		targetString=targetString.replaceFirst("\\[", "");
-		System.out.println(targetString);
-		targetString=targetString.replaceFirst("\\]", "");
-		System.out.println(targetString);
-		surplusString =  targetString;
-		{
+		int startDelimiterOffset,endDelimiterOffset;
+			startDelimiterOffset =  targetString.indexOf(startDelimiter.charAt(0));
+			endDelimiterOffset = targetString.indexOf(endDelimter.charAt(0));
+
+			result = targetString.substring(startDelimiterOffset+1, endDelimiterOffset);
+			head = targetString.substring( 0,startDelimiterOffset );
+			tail = targetString.substring( endDelimiterOffset+1, targetString.length() );
+			surplusString =  head.concat( tail );
 			tokenCount++;
 			System.out.printf("remain [%s], return[%s], tokenCount[%d]\n",surplusString,result,tokenCount);
-		}
 		return result;
 	}
 	public String hasNext(String delimter){
-		String targetString,result;
+		String targetString,result,tail;
 		targetString =  surplusString;
-		{
-			result = targetString.split(delimter)[0];
-		}
-		surplusString =  targetString.replaceFirst(result, "").replaceFirst(delimter, "");
-		{
+		int delimitterOffset;
+			delimitterOffset = targetString.indexOf(delimter.charAt(0));
+			result = targetString.substring(0 ,delimitterOffset );
+		    tail = targetString.substring( delimitterOffset+1,targetString.length()  );
+		    surplusString = tail;
 			tokenCount++;
 			System.out.printf("remain [%s], return[%s], tokenCount[%d]\n",surplusString,result,tokenCount);
-		}
 		return result;
 	}
 	
