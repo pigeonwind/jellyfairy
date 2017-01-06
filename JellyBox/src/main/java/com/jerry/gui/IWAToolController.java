e7166a005ffc8d8908c391e562f085393d2629dd
pigeonwind <pigeonwind@gmail.com>
2017-01-06 10:30:21 +0900

    Lamda 사용 연습3~~

---
+++ JellyBox/src/main/java/com/jerry/gui/IWAToolController.java
@@ -0,0 +1,11 @@
+package com.jerry.gui;
+
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * Created by jerryDev on 2017. 1. 6..
+ */
+public class IWAToolController {
+    public SplitPane mainPane;
+}
