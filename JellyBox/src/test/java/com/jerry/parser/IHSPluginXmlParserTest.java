df5048dbd3859971ae8350c4b497930ddc5ec4e7
jerry <jerry@jerry-THINK>
2017-01-10 19:57:24 +0900

    IHSPluginXmlParserTest -

--- JellyBox/src/test/java/com/jerry/parser/IHSPluginXmlParserTest.java
+++ JellyBox/src/test/java/com/jerry/parser/IHSPluginXmlParserTest.java
@@ -4,19 +4,23 @@ import static java.lang.System.out;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
-import com.jerry.util.function.StringParser;
 import org.junit.*;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.*;
+
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 
 /**
@@ -24,23 +28,17 @@ import java.util.stream.Collectors;
  */
 public class IHSPluginXmlParserTest {
     private String ihsPluginFilePath;
-
+    private List<String> serverList,clusterList;
+    private List<Map<String,String>> serverClusterMapList,uriGorupList,uriList;
     @Before
     public void setUp() throws Exception {
         ihsPluginFilePath = System.getProperty("user.dir")+"/testResource/"+"plugin-cfg-gerp.xml";
+        initDummyData();
     }
 
-    @After
-    public void tearDown() throws Exception {
-
-    }
-
-    @Test
-    public void xmlParsing_IhsWasMap_Test() throws Exception {
-        out.printf( "========= %sTest() START =========\n", "xmlParsing_IhsWasMap_" );
-        // given
-
-        InputSource intputSrource;{
+    private void initDummyData() throws SAXException, IOException, ParserConfigurationException {
+		// TODO Auto-generated method stub
+    	InputSource intputSrource;{
             intputSrource= new InputSource( new FileReader( ihsPluginFilePath ) );
         }
         Document document;{
@@ -77,69 +75,102 @@ public class IHSPluginXmlParserTest {
                 throw new RuntimeException( e );
             }
         };
-
-
-        BiFunction<String,String,List<String>> getList = (expression,attrName) -> {
-            ArrayList<String> list = new ArrayList<>(  );
-            NodeList nodeList = compileExpression.andThen( toNodeListFunction ).apply( expression );
-            for (int index = 0; index < nodeList.getLength(); index++) {
+        BiFunction<NodeList, String, List<String>> toStringList = (nodeList,attrName)->{
+        	ArrayList<String> list = new ArrayList<>(  );
+        	for (int index = 0; index < nodeList.getLength(); index++) {
                 list.add( nodeList.item( index ).getAttributes().getNamedItem(attrName).getTextContent() );
             }
-            return list;
+        	return list;
+    	};
+        BiFunction<String,String,List<String>> getList = (expression,attrName) -> {
+            return toStringList.apply(compileExpression.andThen( toNodeListFunction ).apply( expression ), attrName);
         };
-        List<String> serverList;
         {// get ServerList
             String attributeName="Name";
             String expression = "//*/ServerCluster/Server";
             serverList = getList.apply( expression,attributeName );
+            System.out.println("=========== server list ===========");
             out.println(serverList);
         }// ihs-was map
         String ihsNameFilter = "-(.*?)\\.xml";
-
-        List<String> clusterList;
         {// was-server map
-            //route >> clusterName -  UrigroupName
             String expression = "//*/ServerCluster";
             String attrName = "Name";
             clusterList =  getList.apply( expression,attrName );
+            System.out.println("=========== cluster list ===========");
             out.println(clusterList);
         }
-        List<Map<String,String>> uriGorupList;
+        {
+        	String clusterExpressoin =  "//*/ServerCluster";
+        	UnaryOperator<String> serverExpressionOperation = clusterName->String.format("//*/ServerCluster[@Name=\"%s\"]/Server",clusterName);
+        	String attrName="Name";
+        	Function<String,List<String>> getClusterList=compileExpression.andThen(toNodeListFunction)
+        					 											  .andThen( nodeList->toStringList.apply(nodeList,attrName));
+        	System.out.println("=========== cluster - server map list ===========");
+        	serverClusterMapList = getClusterList.apply(clusterExpressoin).stream()
+        																  .flatMap(serverCluster->{
+        																							String serverExpression=serverExpressionOperation.apply(serverCluster);
+        																							return getList.apply( serverExpression,attrName )
+        																										  .stream()
+        																										  .map(server->{Map<String,String> map=new HashMap<>();map.put("ServerCluster", serverCluster);map.put("Server", server);return map; }); } )
+        			  													  .peek(out::println).collect(Collectors.toList());
+        }
+        BiFunction<Node,String,String> getNodeAttrContent= (node,attrName)-> node==null?"null":node.getAttributes().getNamedItem(attrName).getTextContent();
         {// uriGroup
             String attrName="UriGroup";
-//            String expression =String.format("//*/Route[@ServerCluster=%s]",serverClusterValue);
             Function<String,String> configureExpression = str->String.format("//*/Route[@%s=\"%s\"]","ServerCluster",str);
-
-            Function<String,String> getUriGroupFunction =configureExpression.andThen(compileExpression).andThen( toNodeFunction )
-                                                .andThen( node->node.getAttributes().getNamedItem(attrName).getTextContent() )
-                                                ;
+            Function<String,String> getUriGroupFunction =configureExpression.andThen(compileExpression)
+            																.andThen( toNodeFunction )
+            																.andThen( node-> getNodeAttrContent.apply(node, attrName) );
             Function<String,Map<String,String>> mapFunction = serverCluster->{
                 HashMap<String,String> reulst = new HashMap<>(  );
                 reulst.put( "ServerCluster",serverCluster );
-                reulst.put( "uriGroup",getUriGroupFunction.apply( serverCluster ) );
+                reulst.put( "UriGroup",getUriGroupFunction.apply( serverCluster ) );
 
                 return reulst;
             };
-
-            uriGorupList=clusterList.stream().peek( out::println )
-                                            .map( mapFunction )
+            System.out.println("=========== uri group list ===========");
+            uriGorupList=clusterList.stream().map( mapFunction )
                                             .peek( out::println )
                                             .collect( Collectors.toList() );
         }
+        {// uriGroups - uri
+        	String attrName="Name";
+        	UnaryOperator<String> uriConfigureExpression = uriGroupName->String.format("//*/UriGroup[@Name=\"%s\"]/Uri", uriGroupName);
+        	Function<String,List<String>> getUriListFunction = uriConfigureExpression.andThen(compileExpression)
+        																	   .andThen(toNodeListFunction)
+        																	   .andThen( nodeList->toStringList.apply(nodeList,attrName));
+        	BiFunction<String,String,Map<String,String>> toMap =(uriGroup,uri) ->{
+        		HashMap<String,String> result=new HashMap<>();
+        		result.put("UriGroup", uriGroup);
+        		result.put("Uri", uri);
+        		return result;
+        	};
+        	System.out.println("=========== uri list ===========");
+        	uriList=uriGorupList.stream().flatMap(map-> {
+        								String uriGrup = map.get("UriGroup");
+        						  		return getUriListFunction.apply(uriGrup).stream().map(uri-> toMap.apply(uriGrup, uri));
+        						  })
+        						 .peek(out::println)
+        						 .collect(Collectors.toList());
+        }
+        
+	}
 
-        Object expected = serverList.stream().map( (serverName)->
-                                                    {Map<String,Object> inputDataMap = new HashMap<>(  );
-                                                    inputDataMap.put( "wasServerName",serverName );
-                                                    inputDataMap.put("ihsServerName", DefaultParser.REGEX_PARSE_OPERATOR.parse( ihsPluginFilePath,ihsNameFilter ));
-                                                    return inputDataMap    ;} ).collect( Collectors.toList() );
+	@After
+    public void tearDown() throws Exception {
+		
+    }
 
-
+    @Test
+    public void getServerListTest() throws Exception {
+        out.printf( "========= %sTest() START =========\n", "xmlParsing_IhsWasMap_" );
+        // given
+        String moduleName ="testModule";
+        Object expected =null;
         // when
         Object actual = null;
         // then
         assertThat( actual, is( expected ) );
-
     }
-
-
 }
